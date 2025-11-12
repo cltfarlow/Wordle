d076611c047711964021ffb50546b4d059ab30bc
KleinMax1 <kleinm8@my.erau.edu>
2025-11-12 16:21:53 -0700

    Add files via upload

---
+++ colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RESET "\033[0m"
+
+#define GREEN_BG "\033[48;2;83;141;78m"
+#define YELLOW_BG "\033[48;2;181;159;59m"
+#define GREY_BG "\033[48;2;;58;58;58m"
+
+#define WHITE_TXT "\033[97m"
+#define BOLD_TXT "\033[1m"
+
+#endif
---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef CHECK_LETTER_H   // include guard start
+#define CHECK_LETTER_H
+
+#define FIVE 5
+
+void check_letter(const char guess[], const char answer[], int colors[]);
+
+void print_color(int colors[], char guess[]); 
+
+
+#endif 
