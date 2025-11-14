40bb330dec934bd73be6e19bd93a2f2f3576eaf1
Salaze10 <salaze10@my.erau.edu>
2025-11-14 15:32:35 -0700

    Update play.c
    updated to print colored backgrounds

--- play.c
+++ play.c
@@ -241,7 +241,7 @@ void print_sep(Game *g)
   printf("+\n");
 }
 
-void print_guess_row(char *guess, Game *g)
+void print_guess_row(char *guess, int colors[], Game *g)
 {
   int i;
   int n = g->word_length;
@@ -260,10 +260,36 @@ void print_guess_row(char *guess, Game *g)
       ch = ' ';
     }
 
-    printf(" %c |", (char)toupper((unsigned char)ch));
+    ch = (char)toupper((unsigned char)ch);
+
+    switch (colors[i])
+    {
+    case 0:
+      printf(GREY_BG WHITE_TXT BOLD_TXT " %c " RESET, ch);
+      break;
+    case 1:
+      printf(YELLOW_BG WHITE_TXT BOLD_TXT " %c " RESET, ch);
+      break;
+    case 2:
+      printf(GREEN_BG WHITE_TXT BOLD_TXT " %c " RESET, ch);
+      break;
+    default:
+      /* fall back to no color if colors[i] is something else */
+      printf(" %c ", ch);
+      break;
+    }
+
+    printf("|");
   }
   printf("\n");
 }
+
+void print_color(int colors[], char guess[], Game *g)
+{
+  print_sep(g);
+  print_guess_row(guess, colors, g);
+}
+
 
 void uppercase(char *s)
 {
@@ -313,3 +339,4 @@ int get_int_choice(char *prompt, int min_value, int max_value)
   clear_line();
   return value;
 }
+
