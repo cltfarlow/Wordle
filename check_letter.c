ae8991ee9bf8faeb949e3ba8c0ca9fd008bb98d6
Max Klein <kleinm8@my.erau.edu>
2025-11-14 15:28:20 -0700

    Check_Letter.c update to fit varying word lengths

--- check_letter.c
+++ check_letter.c
@@ -3,27 +3,63 @@
 #include <string.h> 
 #include "colors.h"
 #include "functions.h"
-
-#define FIVE 5
-
-void check_letter(const char guess[], const char answer[], int colors[]) {
+void check_letter_(const char guess[], const char answer[], int colors[], Game *g) {
 	int i; 
   int j;
-  for (i = 0; i < FIVE; i++)
+  for (i = 0; i < g->word_length; i++){
     colors[i] = 0;
-  int check_duplicates[FIVE] = {0};
-	for (i = 0; i < FIVE; i++){
-		if (guess[i] == answer[i]) {
-		  colors[i] = 2;
-      check_duplicates[i] = 1; 
-      continue; 
-		}else if (guess[i] != answer[i]){
-      for (j = 0; j < FIVE; j++){
-        if (guess[i] == answer[j] && check_duplicates[j] == 0){
-          colors[i] = 1;
-          check_duplicates[j] = 1; 
-        } 
+    switch(g->word_length){
+      case 5: 
+      int check_duplicates[FIVE] = {0};
+      for (i = 0; i < g->word_length; i++){
+		    if (guess[i] == answer[i]) {
+		      colors[i] = 2;
+          check_duplicates[i] = 1; 
+          continue; 
+		    }else if (guess[i] != answer[i]){
+          for (j = 0; j < g->word_length; j++){
+            if (guess[i] == answer[j] && check_duplicates[j] == 0){
+              colors[i] = 1;
+              check_duplicates[j] = 1; 
+            } 
+          }
+        }
       }
+      break; 
+      case 8: 
+        int check_duplicates[EIGHT] = {0};
+      	for (i = 0; i < g->word_length; i++){
+		      if (guess[i] == answer[i]) {
+		        colors[i] = 2;
+            check_duplicates[i] = 1; 
+            continue; 
+		      }else if (guess[i] != answer[i]){
+            for (j = 0; j < g->word_length; j++){
+              if (guess[i] == answer[j] && check_duplicates[j] == 0){
+                colors[i] = 1;
+                check_duplicates[j] = 1; 
+              } 
+            }
+          }
+        }
+      break;
+      case 10:
+        int check_duplicates[TEN] = {0};
+        for (i = 0; i < g->word_length; i++){
+		      if (guess[i] == answer[i]) {
+		        colors[i] = 2;
+            check_duplicates[i] = 1; 
+            continue; 
+		      }else if (guess[i] != answer[i]){
+            for (j = 0; j < g->word_length; j++){
+              if (guess[i] == answer[j] && check_duplicates[j] == 0){
+                colors[i] = 1;
+                check_duplicates[j] = 1; 
+              } 
+            }
+          }
+        }
+      break;
     }
   }
 }
