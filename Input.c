8762ec0591ee1fb6accfb391350e008d227f992e
Max Klein <kleinm8@my.erau.edu>
2025-11-19 17:19:12 -0700

    final fix?

--- Input.c
+++ Input.c
@@ -17,69 +17,90 @@
 */
 
 void check_letter(char guess[], int colors[], char answer[], int word_length) {
-	int i; 
-  int j;
-  for (i = 0; i < word_length; i++){
-    colors[i] = 0;
-  }
-    switch(word_length){
-      case 5: {
-      int check_duplicates_FIVE[FIVE] = {0};
-      for (i = 0; i < word_length; i++){
-		    if (guess[i] == answer[i]) {
-		      colors[i] = 2;
-          check_duplicates_FIVE[i] = 1; 
-          continue; 
-		    }else if (guess[i] != answer[i]){
-          for (j = 0; j < word_length; j++){
-            if (guess[i] == answer[j] && check_duplicates_FIVE[j] == 0){
-              colors[i] = 1;
-              check_duplicates_FIVE[j] = 1; 
-            } 
-          }
-        }
-      }
-      break; 
-      }
-      case 8: {
-        int check_duplicates_EIGHT[EIGHT] = {0};
-      	for (i = 0; i < word_length; i++){
-		      if (guess[i] == answer[i]) {
-		        colors[i] = 2;
-            check_duplicates_EIGHT[i] = 1; 
-            continue; 
-		      }else if (guess[i] != answer[i]){
-            for (j = 0; j < word_length; j++){
-              if (guess[i] == answer[j] && check_duplicates_EIGHT[j] == 0){
-                colors[i] = 1;
-                check_duplicates_EIGHT[j] = 1; 
-              } 
-            }
-          }
-        }
-      break;
-      }
-      case 10:{
-        int check_duplicates_TEN[TEN] = {0};
-        for (i = 0; i < word_length; i++){
-		      if (guess[i] == answer[i]) {
-		        colors[i] = 2;
-            check_duplicates_TEN[i] = 1; 
-            continue; 
-		      }else if (guess[i] != answer[i]){
-            for (j = 0; j < word_length; j++){
-              if (guess[i] == answer[j] && check_duplicates_TEN[j] == 0){
-                colors[i] = 1;
-                check_duplicates_TEN[j] = 1; 
-              } 
-            }
-          }
-        }
-      break;
-      }
-    }
-  }
+    int i; 
 
+    int j;
+    for (i = 0; i < word_length; i++){
+    colors[i] = 0;
+    }
+    switch(word_length){
+        case 5: {
+            int check_duplicates_FIVE[FIVE] = {0};
+            for (i = 0; i < word_length; i++){
+                if (guess[i] == answer[i]) {
+                    colors[i] = 2;
+                    check_duplicates_FIVE[i] = 1; 
+                }
+                printf("%d", check_duplicates_FIVE[i]);
+
+            }
+            for (i = 0; i < word_length; i++){
+                for (j = 0; j < word_length; j++){
+                    if (guess[i] != answer[i]){
+                        for (j = 0; j < word_length; j++){
+                            if ((guess[i] == answer[j]) && (check_duplicates_FIVE[j] == 0)){
+                                printf("%d", check_duplicates_FIVE[i]);
+                                colors[i] = 1;
+                                check_duplicates_FIVE[j] = 1; 
+                            } 
+                        }
+                    }
+                } 
+            }
+            break;
+        }
+        case 8: {
+            int check_duplicates_EIGHT[EIGHT] = {0};
+            for (i = 0; i < word_length; i++){
+                if (guess[i] == answer[i]) {
+                    colors[i] = 2;
+                    check_duplicates_EIGHT[i] = 1; 
+                }
+                printf("%d", check_duplicates_EIGHT[i]);
+
+            }
+            for (i = 0; i < word_length; i++){
+                for (j = 0; j < word_length; j++){
+                    if (guess[i] != answer[i]){
+                        for (j = 0; j < word_length; j++){
+                            if ((guess[i] == answer[j]) && (check_duplicates_EIGHT[j] == 0)){
+                                printf("%d", check_duplicates_EIGHT[i]);
+                                colors[i] = 1;
+                                check_duplicates_EIGHT[j] = 1; 
+                            } 
+                        }
+                    }
+                } 
+            }
+            break;
+      }
+        case 10:{
+            int check_duplicates_FIVE[FIVE] = {0};
+            for (i = 0; i < word_length; i++){
+                if (guess[i] == answer[i]) {
+                    colors[i] = 2;
+                    check_duplicates_FIVE[i] = 1; 
+                }
+                printf("%d", check_duplicates_FIVE[i]);
+
+            }
+            for (i = 0; i < word_length; i++){
+                for (j = 0; j < word_length; j++){
+                    if (guess[i] != answer[i]){
+                        for (j = 0; j < word_length; j++){
+                            if ((guess[i] == answer[j]) && (check_duplicates_FIVE[j] == 0)){
+                                printf("%d", check_duplicates_FIVE[i]);
+                                colors[i] = 1;
+                                check_duplicates_FIVE[j] = 1; 
+                            } 
+                        }
+                    }
+                } 
+            }
+            break;
+        }
+    }
+}
 void read_attempt(char *guess, int word_length, char word_list[])
 {
   int length;
