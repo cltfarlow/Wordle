efa42a30af90d8c331ea19d4d31ca5e6f25ef14d
cltfarlow <farlowc1.erau.edu>
2025-11-21 15:24:56 -0700

    standardize var names

--- CheckWord.h
+++ CheckWord.h
@@ -1,8 +1,8 @@
 #ifndef CHECKWORD_H
 
-int isValidWord(char bigA[], char guess[], int row, int col);
-int initArray(char bigA[], int row, int col);
-void generateRandWord(char word[], char bigA[], int row, int col);
+int is_valid_word(char allowed_words[], char guess[], int row, int col);
+int init_array(char array[], int row, int col);
+void generate_rand_word(char word[], char possible_words[], int row, int col);
 void read_attempt(char *guess, int word_length, char word_list[]);
 int all_letters(char *s);
 #endif
