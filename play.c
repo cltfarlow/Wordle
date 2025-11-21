0b0893c42c3a49d9fd92bb056feb385f4e8cb4f1
Salaze10 <salaze10@my.erau.edu>
2025-11-21 16:04:28 -0700

    Update play.c
    FINAL FINAL FINAL FINAL

--- play.c
+++ play.c
@@ -8,25 +8,36 @@
 #include "Output.h"
 
 /*
-  File: Menu.c
+  File: play.c
   Author: Emilina Salazar
+  Debugged: Christopher Farlow, Max Klein
   Assignment: Wordle Project 11/22/2025
   Date: 11/22/2025
   References: notes, google
 */
 
+/* function pre-declaration */
 void play_easy(Game *g);
 void play_medium(Game *g);
 void play_hard(Game *g);
 void play_loop(Game *g, int colors[]);
-void play_easy(Game *g)
 
+/* 
+prints empty game board, then calls play look with game settings and color checking array
+play easy function - normal wordle settings
+  arguments:
+  game *g - struct for game
+          g.max_tries = max tires player gets = 5
+          g.word_length = guess word length = 6
+*/
+void play_easy(Game *g)
 {
   char line[64];
   int colors_FIVE[5];
   generateRandWord(g->answer, g->smaller_word_list, SMALLER_FIVE_LETTER_LEN, FIVE);
   printf("\n[Play] Starting game with word length %d and %d max tries.\n",
          g->word_length, g->max_tries);
+  
   printf("\n+---+---+---+---+---+\n");
   printf("|   |   |   |   |   |\n");
   printf("+---+---+---+---+---+\n");
@@ -47,7 +58,15 @@ void play_easy(Game *g)
   }
 }
 
-
+/* 
+prints empty game board, then calls play look with game settings and color checking array
+play medium function - normal wordle settings
+  8 letter words 6 tries
+  arguments:
+  game *g - struct for game
+          g.max_tries = max tires player gets = 8
+          g.word_length = guess word length = 6
+*/
 void play_medium(Game *g)
 {
   char line[64];
@@ -76,7 +95,15 @@ void play_medium(Game *g)
   {
   }
 }
-
+/* 
+prints empty game board, then calls play look with game settings and color checking array
+play hard function - normal wordle settings
+  10 letter words 10 tries
+  arguments:
+  game *g - struct for game
+          g.max_tries = max tires player gets = 10
+          g.word_length = guess word length = 10
+*/
 void play_hard(Game *g)
 {
   char line[64];
@@ -106,6 +133,19 @@ void play_hard(Game *g)
   }
 }
 
+
+/* 
+play loop - takes in guess, loops through for all tries, prints guess to board
+arguments:
+game *g - struct for game
+          g.max_tries = max tires player gets
+          g.word_length = guess word length
+int color - color array
+            used to determine the background color for letters in the guess
+            0 = gray = wrong letter
+            1 = orange = right letter wrong spot
+            2 = green = right letter right spot
+*/
 void play_loop(Game *g, int colors[])
 {
   char guess[50];
@@ -145,7 +185,7 @@ void play_loop(Game *g, int colors[])
       }
       empty[g->word_length] = '\0';
       check_letter(guess, color_board[attempt], g->answer, g->word_length); 
-      print_sep(g->word_length);
+      print_sep(g->word_length); 
       for (i = 0; i < g->max_tries; i++)
       {
         if (board[i][0] != '\0')
@@ -155,22 +195,23 @@ void play_loop(Game *g, int colors[])
         else
         {
           print_guess_row(empty, color_board[i], g->word_length); 
-        } /* print empty rows*/
+        } /* print rows unused guesses*/
         print_sep(g->word_length);
       }
       if (strcmp(g->answer, guess) == 0)
       {
         printf("\ncongragulations you won!");
         return; 
-      }
+      } /* if player guesses correct guess end game and tell them they won */
     }
   }
   if (strcmp(guess, g->answer) != 0)
   {
     printf("\nNo more tries left. You lost.");
     printf("\nThe answer was: %s.", g->answer);
-  }
+  } /* if player runs out of guesses and got answer wrong tell them they lost */ 
 }
+
 
 
 
